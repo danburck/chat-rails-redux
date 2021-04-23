c09c16999f074dd3bd5b7683476f61445472fdd3
danburck <daniel.m.burckhardt@gmail.com>
2021-04-23 17:36:44 +0200

    Feat (API v1): Refactor as_json method in message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,15 @@ class Message < ApplicationRecord
   validates :content, presence: true, length: { minimum: 1 }
   validates :user, presence: true
   validates :channel, presence: true
+
+  def as_json(_options = {})
+    author = user.email.match(/(?<=@)[^.]+/).to_s
+
+    {
+      id: id,
+      author: author,
+      content: content,
+      created_at: created_at
+    }
+  end
 end
